dc43a0ba930c97fb8407021dbfeaa8340e7f7819
jobin <jobin@LAPTOP-KC34AG1S>
2023-12-03 20:25:16 +0530

    Adding my new project to git repository

---
+++ TestGitNewProject/src/main/java/Pack/ReadExcel.java
@@ -0,0 +1,16 @@
+package Pack;
+
+import java.io.IOException;
+
+public class ReadExcel {
+
+	public static void main(String[] args) throws IOException {
+		Excel e=new Excel();
+		String a=e.readData(0,0);
+		System.out.println("Username is:"+a);
+		String b=e.readData(0,1);
+		System.out.println("Username is:"+b);
+
+	}
+
+}
